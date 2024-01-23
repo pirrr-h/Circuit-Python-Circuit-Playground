dcb77dea4363d30fc6315a42c8776573bdff264a
pirrr-h <156934647+pirrr-h@users.noreply.github.com>
2024-01-23 07:57:00 -0800

    Add files via upload

---
+++ CPX-NeoPixel-2-LED-Blink.py
@@ -0,0 +1,13 @@
+
+import time
+import board
+import neopixel
+
+# Calls the circuit Python neopixel library, specifies the default board 
+# pins for the Neopixels, and the number of neopixels to access.  Returns a 
+# list 'pixels' of indexable neopixles
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
+
+while True:
+    pixels[0] = (0, 255, 0)
+    time.sleep(0.5)
